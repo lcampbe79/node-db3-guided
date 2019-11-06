352281411d0299042218d8e00d25769bd37ab970
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-06 12:11:23 -0800

    Forgot to save.

--- users/user-router.js
+++ users/user-router.js
@@ -30,34 +30,34 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// router.post('/', (req, res) => {
-//   const userData = req.body;
+router.post('/', (req, res) => {
+  const userData = req.body;
 
-//   db('users').insert(userData)
-//   .then(ids => {
-//     res.status(201).json({ created: ids[0] });
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to create new user' });
-//   });
-// });
+  db('users').insert(userData)
+  .then(ids => {
+    res.status(201).json({ created: ids[0] });
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to create new user' });
+  });
+});
 
-// router.put('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const changes = req.body;
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
-//   db('users').where({ id }).update(changes)
-//   .then(count => {
-//     if (count) {
-//       res.json({ update: count });
-//     } else {
-//       res.status(404).json({ message: 'Could not find user with given id' });
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to update user' });
-//   });
-// });
+  db('users').where({ id }).update(changes)
+  .then(count => {
+    if (count) {
+      res.json({ update: count });
+    } else {
+      res.status(404).json({ message: 'Could not find user with given id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to update user' });
+  });
+});
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
